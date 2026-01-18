e8a4a52cf29e49f1a194694a6750b1867fef13cb
lilly selzer <134238217+liselzer@users.noreply.github.com>
2026-01-17 20:41:57 -0800

    I83

--- sketch.js
+++ sketch.js
@@ -58,7 +58,7 @@ function getTransform() {
   let bounds = getBounds();
 
   // tweak these to taste
-  let padding = 80;
+  let padding = 90;
 
   let scaleFactor = min(
     (width - padding) / bounds.width,
